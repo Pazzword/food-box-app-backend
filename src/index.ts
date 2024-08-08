ba8e42ea4a143512bd02af0ca007c09c78958cfa
Anzor <anzor.pozner@gmail.com>
2024-08-08 04:18:39 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express"
+import cors from "cors"
+import "dotenv/config"
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+mongoose
+.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(() => console.log("connected to Database!"));
+
+
+{/*ADD MIDLEWARE - IT WILL AUTOMATICALY CONVERT ANY REQUEST 
+    WE MAKE TO API SERVER TO JSON*/}
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({ message: "Health OK" });
+})
+
+app.use("/api/my/user", myUserRoute);
+
+app.listen(7000, ()=> {
+    console.log("server started on localhost:7000")
+})
